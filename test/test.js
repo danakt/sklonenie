315511cab93fc381dd94898b97b63b014018bcb8
Danakt Frost <mail@danakt.ru>
2017-07-26 23:53:44 +0300

    Фикс тестов

--- test/test.js
+++ test/test.js
@@ -21,20 +21,20 @@ const CASES = [
  */
 describe('Склонение имён', () => {
     // Перебор имён
-    for (let i = 0; i < firstnames.length; i++) {
-        describe(firstnames[i][0], () => {
+    for (let c = 0; c < CASES.length; c++) {
+        describe(CASES[c], () => {
             // Перебор падежей
-            for (let c = 0; c < CASES.length; c++) {
-                it(CASES[c], () => {
+            for (let i = 0; i < firstnames.length; i++) {
+                it(firstnames[i][c], () => {
                     const gender = firstnames[i][6]
-                    const firstname = sklonenie(
-                        firstnames[i][0],
-                        '',
-                        '',
-                        gender
-                    ).firstname[c]
 
-                    expect(firstname).to.equal(firstnames[i][c])
+                    expect(
+                        sklonenie(firstnames[i][0], '', '', gender).firstname[c]
+                    ).to.equal(firstnames[i][c])
+
+                    expect(
+                        sklonenie.firstname(firstnames[i][0], gender)[c]
+                    ).to.equal(firstnames[i][c])
                 })
             }
         })
@@ -45,21 +45,22 @@ describe('Склонение имён', () => {
  * Склонение отчеств
  */
 describe('Склонение отчеств', () => {
-   // Перебор имён
-    for (let i = 0; i < middlenames.length; i++) {
-        describe(middlenames[i][0], () => {
+    // Перебор имён
+    for (let c = 0; c < CASES.length; c++) {
+        describe(CASES[c], () => {
             // Перебор падежей
-            for (let c = 0; c < CASES.length; c++) {
-                it(CASES[c], () => {
+            for (let i = 0; i < middlenames.length; i++) {
+                it(middlenames[i][c], () => {
                     const gender = middlenames[i][6]
-                    const middlename = sklonenie(
-                        '',
-                        middlenames[i][0],
-                        '',
-                        gender
-                    ).middlename[c]
 
-                    expect(middlename).to.equal(middlenames[i][c])
+                    expect(
+                        sklonenie('', middlenames[i][0], '', gender)
+                            .middlename[c]
+                    ).to.equal(middlenames[i][c])
+
+                    expect(
+                        sklonenie.middlename(middlenames[i][0], gender)[c]
+                    ).to.equal(middlenames[i][c])
                 })
             }
         })
@@ -69,24 +70,24 @@ describe('Склонение отчеств', () => {
 /** ----------------------------------------------------------------------------
  * Склонение фамилий
  */
-describe('Склонение отчеств', () => {
-  // Перебор имён
-  for (let i = 0; i < lastnames.length; i++) {
-      describe(lastnames[i][0], () => {
-          // Перебор падежей
-          for (let c = 0; c < CASES.length; c++) {
-              it(CASES[c], () => {
-                  const gender = lastnames[i][6]
-                  const lastname = sklonenie(
-                      '',
-                      '',
-                      lastnames[i][0],
-                      gender
-                  ).lastname[c]
+describe('Склонение фамилий', () => {
+    // Перебор имён
+    for (let c = 0; c < CASES.length; c++) {
+        describe(CASES[c], () => {
+            // Перебор падежей
+            for (let i = 0; i < lastnames.length; i++) {
+                it(lastnames[i][c], () => {
+                    const gender = lastnames[i][6]
 
-                  expect(lastname).to.equal(lastnames[i][c])
-              })
-          }
-      })
-  }
+                    expect(
+                        sklonenie('', '', lastnames[i][0], gender).lastname[c]
+                    ).to.equal(lastnames[i][c])
+
+                    expect(
+                        sklonenie.lastname(lastnames[i][0], gender)[c]
+                    ).to.equal(lastnames[i][c])
+                })
+            }
+        })
+    }
 })
